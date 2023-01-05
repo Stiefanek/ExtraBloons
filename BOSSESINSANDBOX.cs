7694bc1df46ec86bd73e90b390fe601da112b1cc
Steven <107305979+Stiefanek@users.noreply.github.com>
2023-01-05 15:02:36 +0100

    Add files via upload

---
+++ BOSSESINSANDBOX.cs
@@ -0,0 +1,72 @@
+using MelonLoader;
+using BOSSESINSANDBOX;
+using Il2CppAssets.Scripts.Models.Bloons;
+using Il2CppAssets.Scripts.Unity;
+using Harmony;
+using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+
+[assembly: MelonInfo(typeof(ExtraBloons), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+
+
+namespace BOSSESINSANDBOX
+{
+    public class ExtraBloons : MelonMod
+    {
+        public virtual void OnApplicationStart() => MelonLogger.Msg("ExtraBloons Loaded");
+
+        [HarmonyPatch(typeof(Il2CppAssets.Scripts.Unity.UI_New.InGame.BloonMenu.BloonMenu), "CreateBloonButtons")]
+        public class MapLoader_Patch
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(List<BloonModel> sortedBloons)
+            {
+                foreach (BloonModel bloon in (Il2CppArrayBase<BloonModel>)Game.instance.model.bloons)
+                {
+                    if (bloon.baseId == "Bloonarius")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "Vortex")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "Lych")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "MiniLych")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "MiniLychElite")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "Dreadbloon")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "Golden")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "GoldenLead")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonStandard1")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonStandard2")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonStandard3")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonStandard4")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonStandard5")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonElite1")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonElite2")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonElite3")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonElite4")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonElite5")
+                        sortedBloons.Add(bloon);
+                    if (bloon.baseId == "DreadRockBloonElite5")
+                        sortedBloons.Add(bloon);
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+namespace BOSSESINSANDBOX;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "34.3";
+    public const string Version = "0.1.0";
+    public const string Name = "All Extrabloons in Sandbox";
+
+    public const string Description = "Adds all Bloons, Bosses to sandbox like Golden Bloons (They dont give you monkey Money) or Rock Bloons and all Bosses and MiniLych";
+
+    public const string RepoOwner = "Stiefanek"; 
+    public const string RepoName = "ExtraBloons";
+    public const string Icon = "Icon.png";
+    public const bool SquareIcon = true;
+}
